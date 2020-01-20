464f7fc90a8e7f43c92195cf47efb505e891fde2
rmurray2 <rmurray2050@gmail.com>
2020-01-20 15:55:15 +0200

    Update compress_fastq.sh

--- scripts/compress_fastq.sh
+++ scripts/compress_fastq.sh
@@ -9,17 +9,21 @@
 #SBATCH -t 12:20:00
 #SBATCH --mem-per-cpu=1000
 #SBATCH --mail-type=END
+#SBATCH --mail-user=ryan.z.murray@helsinki.fi
+#SBATCH --account=dasroy
 
 source scripts/command_utility.sh
 num_cmnds=$( cmnds_in_file )
 
 module load biokit
 
+echo "Blank lines in sortMeRna files (STAR aligner input should NOT contain blank lines):" >> OUT/blank_lines_check
 for my_file in $1/*.{fastq,fq}
 do
 if [  -f $my_file ]
 then
-
+    #Check for blank lines in sortMeRna files. Star Aligner will not take any lines after a blank line as input. 
+    grep -E --line-number --with-filename '^$' $my_file >> OUT/blank_lines_check
 	echo "gzip  $my_file " >> commands/$num_cmnds"_compress_"$1.txt
 fi
 done
