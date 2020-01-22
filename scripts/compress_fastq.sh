f719a3f62a42c4555358033c6ebe7d8f376dd3db
Ryan Murray <murrayry@puhti-login2.bullx>
2020-01-22 13:07:51 +0200

    blank line checking & removal

--- scripts/compress_fastq.sh
+++ scripts/compress_fastq.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
 # created: Aug 22, 2017 1:55 PM
-# author: dasroy
+# modified: Jan 22, 2019 1:06 PM
+# author: dasroy / murrayry
 #SBATCH -J compress
 #SBATCH -o OUT/compress_out_%j.txt
 #SBATCH -e ERROR/compress_err_%j.txt
@@ -9,23 +10,26 @@
 #SBATCH -t 12:20:00
 #SBATCH --mem-per-cpu=1000
 #SBATCH --mail-type=END
-#SBATCH --mail-user=ryan.z.murray@helsinki.fi
-#SBATCH --account=dasroy
 
 source scripts/command_utility.sh
 num_cmnds=$( cmnds_in_file )
 
 module load biokit
 
-echo "Blank lines in sortMeRna files (STAR aligner input should NOT contain blank lines):" >> OUT/blank_lines_check.txt
+echo "Blank lines in sortMeRna files (STAR aligner input should NOT contain blank lines):" >> ERROR/blank_lines_check.txt
 for my_file in $1/*.{fastq,fq}
 do
 if [  -f $my_file ]
 then
     #Check for blank lines in sortMeRna files. Star Aligner will not take any lines after a blank line as input. 
     #https://www.biostars.org/p/302365/#315729
-    grep -E --line-number --with-filename '^$' $my_file >> OUT/blank_lines_check.txt
-	echo "gzip  $my_file " >> commands/$num_cmnds"_compress_"$1.txt
+    num_blank=$(grep -E --line-number --with-filename "^$" $my_file | wc -l)
+    if [ $num_blank -gt 0 ]
+        then
+        grep -E --line-number --with-filename '^$' $my_file >> ERROR/blank_lines_check.txt
+	sed -i '/^$/d' $my_file
+        fi
+    echo "gzip  $my_file " >> commands/$num_cmnds"_compress_"$1.txt
 fi
 done
 sbatch_commandlist -t 12:00:00 -mem 4000 -jobname compress_array -threads 4 -commands  commands/$num_cmnds"_compress_"$1.txt
