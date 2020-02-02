7cb48000ee52429885052e0ce56878b0bf909279
rmurray2 <rmurray2050@gmail.com>
2020-02-02 17:25:28 +0200

    Update master.sh

--- master.sh
+++ master.sh
@@ -20,7 +20,7 @@ sed -i '/#SBATCH --mail-type=END/a #SBATCH --account=dasroy' $PWD/scripts/*.sh
 sed -i '/#SBATCH --mail-type=END/a #SBATCH --mail-user=ryan.z.murray@helsinki.fi' $PWD/scripts/*.sh
 
 num_files=$(ls $PWD/$1/*{fastq,fastq.gz,fq,fq.gz} | wc -l)
-sed -i "/#SBATCH --mail-type=END/a #SBATCH --array=1-$num_files" $PWD/slurm.sh
+sed -i "/#SBATCH --mail-type=END/a #SBATCH --array=1-$num_files" $PWD/scripts/rsem_array_cmnd.sh
 
 sbatch scripts/fastqc.sh $1
 wait_for_job
