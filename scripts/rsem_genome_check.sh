1ed8d56121f280c9a7cfc8b58ac43c7e73025826
rmurray2 <rmurray2050@gmail.com>
2020-02-02 17:19:45 +0200

    Add files via upload

---
+++ scripts/rsem_genome_check.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -l
+# created: Feb 2, 2020
+# author: rmurray
+#SBATCH -J rsem_calculate
+#SBATCH -o OUT/rsem_out_%j.txt
+#SBATCH -e ERROR/rsem_err_%j.txt
+#SBATCH -p large 
+#SBATCH -n 1
+#SBATCH -t 12:00:00
+#SBATCH --mem-per-cpu=8000
+#SBATCH --mail-type=END
+
+source scripts/command_utility.sh
+num_cmnds=$( cmnds_in_file )
+
+module load gcc star r-env rstudio perl
+
+if [ ! -d rsem_indx  ]; then
+	
+	mkdir rsem_indx
+	overhang=$(( maxReadLength - 1 ))
+
+	echo "/users/murrayry/RSEM/software/RSEM-1.2.25/rsem-prepare-reference --gtf $gene_annotation --star --star-path /appl/soft/bio/star/gcc_9.1.0/2.7.2a/bin --num-threads 8 --star-sjdboverhang $overhang $genome_file rsem_indx/rsem_ref" >> commands/$num_cmnds"_rsem_prepare_reference".txt
+
+	sbatch_commandlist -t 12:00:00 -mem 64000 -jobname rsem_prepare_reference -theads 8 -commands commands/$num_cmnds"_rsem_prepare_reference".txt
+
+	mv *_out_*txt OUT
+	mv *_err_*txt ERROR
+
+fi
+#TODO:
+#add check to see if genome is prepared, 
+
+#THIS doesn't work on sbatch_commandlist for some reason
+#if [ ! -d "$2" ]
+#   then
+#   mkdir $2
+#fi
+#
+#for my_file in $1/*.{,fq.gz}
+#do
+#if [  -f $my_file ]
+#then
+#        filename="${my_file##*/}"
+#        filename="${filename%.*}"
+#  echo "/users/murrayry/RSEM/software/RSEM-1.2.25/rsem-calculate-expression -p 8 --calc-ci --strand-specific --star --star-path /appl/soft/bio/star/gcc_9.1.0/2.7.2a/bin --gzipped-read-file $my_file rsem_indx/rsem_ref $2/rsem_count_$filename" >> commands/$num_cmnds"_RSEM_calculate_expression"$1_commands.txt
+#fi
+#done
+#
+#sbatch_commandlist -t 12:00:00 -mem 24000 -jobname rsem_expression_array -threads 8  -commands commands/$num_cmnds"_RSEM_calculate_expression"$1_commands.txt
+#
+#mv *_out_*txt OUT
+#mv *_err_*txt ERROR
+#
+#source scripts/multiqc_slurm.sh
