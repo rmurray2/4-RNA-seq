93f831ec39bf70f44b9c9b5d00b9a7b604ea57c2
rmurray2 <rmurray2050@gmail.com>
2020-02-04 11:51:28 +0200

    Update diff_exp.sh

--- scripts/diff_exp.sh
+++ scripts/diff_exp.sh
@@ -21,6 +21,17 @@ python scripts/update_sample_description.py $samp_desc
 echo "Rscript scripts/DE_exp.R $samp_desc $biomart_dataset $adj_pval_cutoff" >> commands/$num_cmnds"_DE_exp_commands.txt"
 sbatch_commandlist -t 0:30:00 -mem 4000 -jobname DE_exp -threads 1 -commands commands/$num_cmnds"_DE_exp_commands.txt"
 
+if [ ! -d "$HOME/R_libs" ]
+   then
+   mkdir $HOME/R_libs
+   wget --directory-prefix=$HOME/ https://github.com/mikelove/tximport/archive/master.tar.gz
+   R CMD INSTALL -l $HOME/R_libs/ $HOME/master.tar.gz
+   rm $HOME/master.tar.gz
+fi
+
+echo "Rscript scripts/DE_exp_tximport.R $samp_desc_rsem $biomart_dataset $adj_pval_cutoff $USER" >> commands/$num_cmnds"_DE_exp_rsem_commands.txt"
+sbatch_commandlist -t 0:30:00 -mem 4000 -jobname DE_exp -threads 1 -commands commands/$num_cmnds"_DE_exp_rsem_commands.txt"
+
 mv *_out_*txt OUT
 mv *_err_*txt ERROR
 #Rscript scripts/DE_exp.R $samp_desc $biomart_dataset $adj_pval_cutoff
