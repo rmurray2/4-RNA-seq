59b81ede2c922f612c810c5b077888a8b03a52c8
rmurray2 <rmurray2050@gmail.com>
2020-02-04 16:08:54 +0200

    Update DE_exp.R

--- scripts/DE_exp.R
+++ scripts/DE_exp.R
@@ -3,8 +3,6 @@ args<-commandArgs(TRUE)
 #args[2] is whether human or mouse, hsapiens_gene_ensembl or mmusculus_gene_ensembl
 #args[3] is adjusted pvalue cutoff
 
-p_adjusted = as.numeric(args[3])
-
 setwd("./")
 sampleMetaData <- read.csv(args[1],header = TRUE)
 
