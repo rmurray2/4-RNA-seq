d9341147575abcd9150ed29bad1d45f5f31eea0f
rmurray2 <rmurray2050@gmail.com>
2020-02-06 12:58:26 +0200

    Update diff_exp.sh

--- scripts/diff_exp.sh
+++ scripts/diff_exp.sh
@@ -24,13 +24,13 @@ python scripts/parse_samples.py --data rsem_counts --pairpattern $pairpattern --
 echo "Rscript scripts/DE_exp.R sample_description_for_htseq.csv $biomart_dataset" >> commands/$num_cmnds"_DE_exp_commands.txt"
 sbatch_commandlist -t 0:30:00 -mem 4000 -jobname DE_exp -threads 1 -commands commands/$num_cmnds"_DE_exp_commands.txt"
 
-if [ ! -d "$HOME/R_libs" ]
-   then
-   mkdir $HOME/R_libs
-   wget --directory-prefix=$HOME/ https://github.com/mikelove/tximport/archive/master.tar.gz
-   R CMD INSTALL -l $HOME/R_libs/ $HOME/master.tar.gz
-   rm $HOME/master.tar.gz
-fi
+#if [ ! -d "$HOME/R_libs" ]
+#   then
+#   mkdir $HOME/R_libs
+#   wget --directory-prefix=$HOME/ https://github.com/mikelove/tximport/archive/master.tar.gz
+#   R CMD INSTALL -l $HOME/R_libs/ $HOME/master.tar.gz
+#   rm $HOME/master.tar.gz
+#fi
 
 echo "Rscript scripts/DE_exp_tximport.R sample_description_for_rsem.csv $biomart_dataset $USER" >> commands/$num_cmnds"_DE_exp_rsem_commands.txt"
 sbatch_commandlist -t 0:30:00 -mem 4000 -jobname DE_exp -threads 1 -commands commands/$num_cmnds"_DE_exp_rsem_commands.txt"
