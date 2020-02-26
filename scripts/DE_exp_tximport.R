b6bec0e96ef004b60ee48e61a24ed1c7b57083fc
rmurray2 <rmurray2050@gmail.com>
2020-02-26 09:37:03 +0200

    Update DE_exp_tximport.R

--- scripts/DE_exp_tximport.R
+++ scripts/DE_exp_tximport.R
@@ -41,6 +41,9 @@ ddsHTSeq<- DESeqDataSetFromTximport(txi.rsem, colData = samples, design= ~ pair+
                                         ## will test for fold change between the two conditions (CTRL and SHH) without considering (i.e. correcting for) 'pair' information.
 
 rlogMat<-as.data.frame(assay(rlog(ddsHTSeq)))
+library(stringr)
+no_versions = str_replace(rownames(rlogMat), pattern = ".[0-9]+$",replacement = "")
+rownames(rlogMat) = no_versions
 
 #######################################################
 if (!requireNamespace("biomaRt", quietly = TRUE)) {
@@ -96,6 +99,8 @@ data_DESeq <- DESeq(dermis)
 ## Building the result table
   
 res <- results(data_DESeq, contrast=c("condition", "Exp", "Ctrl"))
+no_versions = str_replace(rownames(res), pattern = ".[0-9]+$",replacement = "")
+rownames(res) = no_versions
 
 #resSig <- subset(res, padj < p_adjusted)
 resSig <- as.data.frame(res)
